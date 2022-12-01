0307f0df1121fb79bc9bebe7e4dec0010c251653
Khongor Bayarsaikhan <b.khongi@gmail.com>
2022-12-01 15:29:55 +0100

    Day 01 - part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -17,12 +17,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var acc = 0
+        val maxes = mutableListOf(0, 0, 0)
+        input.forEachIndexed { index, number ->
+            val calcMax = number.isEmpty() || index == input.lastIndex
+            if (number.isNotEmpty()) {
+                val value = number.toInt()
+                acc += value
+            }
+            if (calcMax) {
+                val maxToReplace = maxes.indexOfFirst { it < acc }
+                if (maxToReplace != -1) {
+                    maxes[maxToReplace] = acc
+                    maxes.sort()
+                }
+                acc = 0
+            }
+        }
+        return maxes.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
