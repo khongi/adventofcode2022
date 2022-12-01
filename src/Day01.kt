a10b9ea4bce9d8d53117d3358190f971be5dd931
Khongor Bayarsaikhan <b.khongi@gmail.com>
2022-12-01 15:03:58 +0100

    Day 01 - part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,19 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var acc = 0
+        var max = 0
+        input.forEach { number ->
+            if (number.isNotEmpty()) {
+                val value = number.toInt()
+                acc += value
+            } else {
+                if (max < acc) {
+                    max = acc
+                }
+                acc = 0
+            }
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +22,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
