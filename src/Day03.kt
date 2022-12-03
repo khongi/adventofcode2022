9d55fc5c82139e2d574164885bf6cb37eaaa3112
Khongor Bayarsaikhan <b.khongi@gmail.com>
2022-12-03 14:43:34 +0100

    Day 03 - part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -7,7 +7,7 @@ fun main() {
         }
     }
 
-    fun String.findCommon(other: String): Set<Char> {
+    fun Set<Char>.findCommon(other: String): Set<Char> {
         return toCharArray().intersect(other.asIterable().toSet())
     }
 
@@ -17,15 +17,32 @@ fun main() {
             val firstHalf = rucksack.substring(0..rucksack.lastIndex/2)
             val secondHalf = rucksack.substring(rucksack.length/2..rucksack.lastIndex)
 
-            sum += firstHalf.findCommon(secondHalf).sumOf { it.getPriority() }
+            sum += firstHalf.asIterable().toSet().findCommon(secondHalf).sumOf { it.getPriority() }
         }
 
         return sum
     }
 
+    fun part2(input: List<String>): Int {
+        val groups = input.chunked(3)
+        var sum = 0
+        groups.forEach { group ->
+            val (first, second, third) = group
+            sum += first
+                .asIterable()
+                .toSet()
+                .findCommon(second)
+                .findCommon(third)
+                .sumOf { it.getPriority() }
+        }
+        return sum
+    }
+
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
+    println(part2(input))
 }
