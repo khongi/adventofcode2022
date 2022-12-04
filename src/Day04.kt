31bdfb592b9d176ceeda05715ea415176e15fc10
Khongor Bayarsaikhan <b.khongi@gmail.com>
2022-12-04 15:26:34 +0100

    Day 04 - part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -12,9 +12,19 @@ fun main() {
         }
     }
 
+    fun part2(input: List<String>): Int {
+        return input.count { line ->
+            val (first, second) = line.split(',').map { it.getRange() }
+            val intersect = first.intersect(second)
+            intersect.isNotEmpty()
+        }
+    }
+
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println(part1(input))
+    println(part2(input))
 }
