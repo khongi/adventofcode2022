c31198c2e5fcec2610d549236348c9bf034d752f
Khongor Bayarsaikhan <b.khongi@gmail.com>
2022-12-10 19:35:38 +0100

    Day 08 - part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -30,6 +30,28 @@ fun main() {
         return leftCheck || rightCheck || topCheck || bottomCheck
     }
 
+    fun calculateScore(subList: List<Int>, height: Int, edgeValue: Int): Int {
+        val closestTall = subList.indexOfFirst { it >= height }
+        return if (closestTall == -1) {
+            edgeValue
+        } else {
+            closestTall + 1
+        }
+    }
+
+    fun List<List<Int>>.scenicScore(rowIndex: Int, colIndex: Int): Int {
+        val height = this[rowIndex][colIndex]
+        val row = this[rowIndex]
+        val col = buildCol(colIndex)
+
+        val leftScore = calculateScore(row.subList(0, colIndex).reversed(), height, colIndex)
+        val rightScore = calculateScore(row.subList(colIndex + 1, row.size), height, row.size - colIndex - 1)
+        val topScore = calculateScore(col.subList(0, rowIndex).reversed(), height, rowIndex)
+        val bottomScore = calculateScore(col.subList(rowIndex + 1, col.size), height, col.size - rowIndex - 1)
+
+        return leftScore * rightScore * topScore * bottomScore
+    }
+
     fun part1(input: List<String>): Int {
         val rows = input[0].length
         val cols = input.size
@@ -44,9 +66,28 @@ fun main() {
         return visibleCount
     }
 
+    fun part2(input: List<String>): Int {
+        val rows = input[0].length
+        val cols = input.size
+        val mtx = buildMatrix(input)
+
+        var max = 0
+        for (r in 1 until rows - 1) {
+            for (c in 1 until cols - 1) {
+                val score = mtx.scenicScore(r, c)
+                if (score > max) {
+                    max = score
+                }
+            }
+        }
+        return max
+    }
+
     val testInput = readInput("Day08_test")
     check(part1(testInput) == 21)
+    check(part2(testInput) == 8)
 
     val input = readInput("Day08")
     println(part1(input))
+    println(part2(input))
 }
