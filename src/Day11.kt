9cc11bac157959f7934b031a941566d0daccdfbf
Khongor Bayarsaikhan <b.khongi@gmail.com>
2022-12-15 11:42:17 +0100

    Day 11 - part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,18 +1,16 @@
 class Monkey(chunk: List<String>) {
-    val items = mutableListOf<Int>()
+    val items = mutableListOf<Long>()
+    val testNumber: Int
     var inspectCount = 0
         private set
-    private val monkeyNumber: Int
     private val left: String
     private val right: String
     private val operation: String
-    private val testNumber: Int
     private val testTrueMonkey: Int
     private val testFalseMonkey: Int
 
     init {
-        monkeyNumber = chunk[0].substringBefore(":").split(" ")[1].toInt()
-        items.addAll(chunk[1].substringAfter(": ").split(", ").map { it.toInt() })
+        items.addAll(chunk[1].substringAfter(": ").split(", ").map { it.toLong() })
         val (l, o, r) = chunk[2].substringAfter("= ").split(" ")
         left = l
         right = r
@@ -22,9 +20,9 @@ class Monkey(chunk: List<String>) {
         testFalseMonkey = chunk[5].substringAfter("monkey ").toInt()
     }
 
-    fun operate(old: Int): Int {
-        val l = if (left == "old") old else left.toInt()
-        val r = if (right == "old") old else right.toInt()
+    fun operate(old: Long): Long {
+        val l = if (left == "old") old else left.toLong()
+        val r = if (right == "old") old else right.toLong()
         return when (operation) {
             "+" -> l + r
             "*" -> l * r
@@ -32,9 +30,9 @@ class Monkey(chunk: List<String>) {
         }
     }
 
-    fun test(item: Int): Int {
+    fun test(item: Long): Int {
         inspectCount++
-        return if (item % testNumber == 0) {
+        return if (item % testNumber == 0L) {
             testTrueMonkey
         } else {
             testFalseMonkey
@@ -43,18 +41,27 @@ class Monkey(chunk: List<String>) {
 }
 
 fun main() {
-    fun Int.calm(): Int = this / 3
+    fun List<Int>.findLcm(): Int {
+        var lcm = max()
+        while (true) {
+            if (count { lcm % it == 0 } != size) {
+                lcm++
+            } else {
+                return lcm
+            }
+        }
+    }
 
-    fun round(monkeys: List<Monkey>) {
+    fun round(monkeys: List<Monkey>, lower: (Long) -> Long) {
         monkeys.forEach { monkey ->
             val iterator = monkey.items.iterator()
             while (iterator.hasNext()) {
                 val item = iterator.next()
                 val newItem = monkey.operate(item)
-                val calmItem = newItem.calm()
-                val monkeyToThrowTo = monkey.test(calmItem)
+                val loweredItem = lower(newItem)
+                val monkeyToThrowTo = monkey.test(loweredItem)
                 iterator.remove()
-                monkeys[monkeyToThrowTo].items.add(calmItem)
+                monkeys[monkeyToThrowTo].items.add(loweredItem)
             }
         }
     }
@@ -62,15 +69,26 @@ fun main() {
     fun part1(input: List<String>): Int {
         val monkeys = mutableListOf<Monkey>()
         input.chunked(7).forEach { monkeys.add(Monkey(it)) }
-        repeat(20) { round(monkeys) }
+        repeat(20) { round(monkeys) { it / 3 } }
         monkeys.sortByDescending { it.inspectCount }
 
         return monkeys[0].inspectCount * monkeys[1].inspectCount
     }
 
+    fun part2(input: List<String>): Long {
+        val monkeys = mutableListOf<Monkey>()
+        input.chunked(7).forEach { monkeys.add(Monkey(it)) }
+        val lcm = monkeys.map { it.testNumber }.findLcm()
+        repeat(10000) { round(monkeys) { it % lcm } }
+        monkeys.sortByDescending { it.inspectCount }
+        return monkeys[0].inspectCount.toLong() * monkeys[1].inspectCount
+    }
+
     val testInput = readInput("Day11_test")
     check(part1(testInput) == 10605)
+    check(part2(testInput) == 2713310158L)
 
     val input = readInput("Day11")
     println(part1(input))
+    println(part2(input))
 }
